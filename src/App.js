99bcaa07221eef95a9bf6146e5258b9fa0811259
unknown <anudeepmarlapalli22@gmail.com>
2022-04-30 22:42:31 +0530

    Added all files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import AdminPanelComp from './AdminPanelComp'
+
+export class App extends Component {
+
+render(){
+  return(
+    <div>
+
+      <AdminPanelComp />
+    </div>
+  )
+}
+}
+
+export default App
